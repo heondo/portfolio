2660577f6bc25dc7cdff03694904d50de4552727
heondo kim <heondokim@gmail.com>
2021-03-20 11:14:50 -0700

    stripe like menu bar, wont have much in there though haha

--- layouts/AppLayout.vue
+++ layouts/AppLayout.vue
@@ -71,10 +71,107 @@
       </div>
 
       <!-- Mobile menu, show/hide based on menu state. -->
-      <div v-show="menuOpen" class="md:hidden" id="mobile-menu">
-        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        I wnat a pop up here with a transition instead
-      </div>
+      <transition
+        enter-active-class="ease-out duration-300"
+        enter-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        enter-to-class="opacity-100 translate-y-0 sm:scale-100"
+        leave-active-class="ease-in duration-200"
+        leave-class="opacity-100 translate-y-0 sm:scale-100"
+        leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      >
+        <div
+          v-show="menuOpen"
+          class="md:hidden fixed z-10 inset-0 overflow-y-auto"
+        >
+          <div class="flex pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <!--
+      Background overlay, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100"
+        To: "opacity-0"
+    -->
+            <div
+              class="fixed inset-0 transition-opacity"
+              aria-hidden="true"
+              @click="menuOpen = !menuOpen"
+            >
+              <div class="absolute inset-0"></div>
+            </div>
+
+            <!-- This element is to trick the browser into centering the modal contents. -->
+            <!-- <span
+              class="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+              >&#8203;</span
+            > -->
+            <!--
+      Modal panel, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        To: "opacity-100 translate-y-0 sm:scale-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100 translate-y-0 sm:scale-100"
+        To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    -->
+            <div
+              class="inline-block align-bottom bg-gray-700 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
+              role="dialog"
+              aria-modal="true"
+              aria-labelledby="modal-headline"
+            >
+              <div>
+                <div
+                  class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100"
+                >
+                  <!-- Heroicon name: outline/check -->
+                  <svg
+                    class="h-6 w-6 text-green-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                </div>
+                <div class="mt-3 text-center sm:mt-5">
+                  <h3
+                    class="text-lg leading-6 font-medium text-white"
+                    id="modal-headline"
+                  >
+                    Payment successful
+                  </h3>
+                  <div class="mt-2">
+                    <p class="text-sm text-gray-200">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Consequatur amet labore.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-5 sm:mt-6">
+                <button
+                  type="button"
+                  class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                >
+                  Go back to dashboard
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
     </nav>
 
     <main class="bg-gray-800 text-white">
