8775290085081823314fb815523a12463bdf4b0c
heondo kim <heondokim@gmail.com>
2021-03-23 17:34:37 -0700

    more responseive

--- components/AppTimeline.vue
+++ components/AppTimeline.vue
@@ -1,87 +1,135 @@
 <template>
-  <span class="grid grid-cols-12 gap-2 gap-y-5 relative">
+  <div class="flex flex-col gap-5">
+    <span class="grid grid-cols-12 gap-2 gap-y-5 relative">
+      <div class="timeline-line"></div>
+      <div class="date">
+        <div class="circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <div>04/18</div>
+      </div>
+      <div class="event">
+        <h3>
+          Data Analyst, Carpe Data -
+          <svg
+            class="h-6 w-6 inline"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+          <p class="text-sm inline">Santa Barbara</p>
+        </h3>
+        <span>
+          <ul class="list-decimal">
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+          </ul>
+        </span>
+      </div></span
+    >
+    <span class="grid grid-cols-12 gap-2 gap-y-5 relative">
+      <div class="timeline-line translate-y-4 transform"></div>
+      <div class="date">
+        <div class="circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <div>08/19</div>
+      </div>
+      <div class="event">
+        <h3>Learning Web Dev</h3>
+        LFZ
+      </div>
+    </span>
+    <span class="grid grid-cols-12 gap-2 gap-y-5 relative">
+      <div class="timeline-line translate-y-4 transform"></div>
+
+      <div class="date">
+        <div class="circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <div>01/20</div>
+      </div>
+      <div class="event">
+        <h3>Data Science Instructor</h3>
+        <span> Tae coding </span>
+      </div>
+    </span>
+    <span class="grid grid-cols-12 gap-2 gap-y-5 relative">
+      <div class="date">
+        <div class="circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <div>08/20</div>
+      </div>
+      <div class="event">
+        <h3>Software Developer</h3>
+        <span> Smartspace ai </span>
+      </div>
+    </span>
+  </div>
+  <!-- <span class="grid grid-cols-12 gap-2 gap-y-5 relative">
     <div class="timeline-line">,</div>
-    <div class="date">
-      <div class="circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </div>
-      <div>04/18</div>
-    </div>
-    <div class="event">
-      <h3>Data Analyst, Carpe Data</h3>
-      <span> Description of event </span>
-    </div>
-    <div class="date">
-      <div class="circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </div>
-      <div>08/19</div>
-    </div>
-    <div class="event">
-      <h3>Learning Web Dev</h3>
-      LFZ
-    </div>
-    <div class="date">
-      <div class="circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </div>
-      <div>01/20</div>
-    </div>
-    <div class="event">
-      <h3>Data Science Instructor</h3>
-      <span> Tae coding </span>
-    </div>
-    <div class="date">
-      <div class="circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </div>
-      <div>08/20</div>
-    </div>
-    <div class="event">
-      <h3>Software Developer</h3>
-      <span> Smartspace ai </span>
-    </div>
-  </span>
+    
+   
+   
+  </span> -->
 </template>
 <script>
 export default {
@@ -91,9 +139,9 @@ export default {
 <style scoped>
 .timeline-line {
   content: '';
-  height: 85%;
-  left: 4.7%;
-  top: 6.2%;
+  height: 100%;
+  left: 3.7%;
+  top: 42.5%;
   z-index: 0;
   opacity: 0.8;
   width: 2px;
@@ -123,12 +171,6 @@ export default {
 
   .event > h3 {
     @apply text-lg;
-  }
-
-  .timeline-line {
-    left: 3.65%;
-    top: 7%;
-    height: 85%;
   }
 }
 
