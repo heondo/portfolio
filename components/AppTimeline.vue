c76d0ea0dfc46a7a95d90857130213527f012e61
heondo kim <heondokim@gmail.com>
2021-03-25 21:56:56 -0700

    fill in bullet points, will need to clean up and highlight key words

--- components/AppTimeline.vue
+++ components/AppTimeline.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex flex-col gap-5">
     <timeline-event>
-      <template #date> 04/12 </template>
+      <template #date> 04/20 </template>
       <h3 class="flex flex-wrap items-center gap-2 mb-1">
-        Data Analyst, Carpe Data -
+        Data Analyst, Carpe Data
         <span
-          ><p class="text-sm inline">Santa Barbara</p>
+          ><p class="text-sm inline">- Santa Barbara</p>
           <svg
             class="map-marker"
             xmlns="http://www.w3.org/2000/svg"
@@ -24,21 +24,22 @@
               stroke-linejoin="round"
               stroke-width="2"
               d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-            /></svg
-        ></span>
+            />
+          </svg>
+        </span>
       </h3>
       <ul class="list-disc pl-4">
         <li>
           Start by creating regular expressions to flag risk characteristics for
-          businesses
+          businesses.
         </li>
         <li>
           Use Python to automate various job tasks like CSV formatting, data
-          cleansing, and other ad hoc solutions
+          cleansing, and other ad hoc solutions.
         </li>
         <li>
           Learned SQL to find business areas where product coverage and accuracy
-          was most lacking in a DB of over 10 million records
+          was most lacking in a DB of over 10 million records.
         </li>
         <li>
           Created data collection process for DA team using Python and SQL.
@@ -102,9 +103,9 @@
     <timeline-event>
       <template #date> 09/19 </template>
       <h3 class="flex flex-wrap items-center gap-2 mb-1">
-        Web Dev Student, LFZ -
+        Web Dev Student, LFZ
         <span
-          ><p class="text-sm inline">Irvine, CA</p>
+          ><p class="text-sm inline">- Irvine, CA</p>
           <svg
             class="map-marker"
             xmlns="http://www.w3.org/2000/svg"
@@ -128,57 +129,55 @@
       </h3>
       <span>
         <ul class="list-disc pl-4">
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
+          <li>
+            Begin with learning HTML, CSS, and JavaScript (ES5), Flexbox ftw,
+            and jQuery.
+          </li>
+          <li>
+            Learn React and ES6 once familiarizing ourself with vanilla
+            JavaScript, trying to understand the basics of web dev.
+          </li>
+          <li>
+            Taught SQL and PHP for developing backends for our applications. I
+            was familiar with SQL so that was straightforward, PHP was
+            interesting and learned Express.js as a challenge for my final
+            project
+          </li>
         </ul>
       </span>
     </timeline-event>
     <timeline-event>
       <template #date> 01/20 </template>
       <h3 class="flex flex-wrap items-center gap-2 mb-1">
-        Data Analyst, Carpe Data -
-        <span
-          ><p class="text-sm inline">Santa Barbara</p>
-          <svg
-            class="map-marker"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-            />
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-            /></svg
-        ></span>
+        Data Science Instructor, Tae Coding
       </h3>
       <span>
         <ul class="list-disc pl-4">
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
+          <li>
+            Learned Angular, MongoDB, and AWS Amplify to assist in the
+            development of an internal application for class management.
+          </li>
+          <li>
+            Brushing up my Pandas and Python skills to teach high schoolers
+            preparing for programming olympiads.
+          </li>
+          <li>
+            Private tutor and lead group classes for statistics and data
+            science.
+          </li>
+          <li>
+            Help more than 20 students receive one or more certificates from
+            Edx.
+          </li>
         </ul>
       </span>
     </timeline-event>
     <timeline-event :hideTimeline="true">
-      <template #date> 04/20 </template>
+      <template #date> 08/20 </template>
       <h3 class="flex flex-wrap items-center gap-2 mb-1">
-        Data Analyst, Carpe Data -
+        Software Engineer, SmartSpace AI
         <span
-          ><p class="text-sm inline">Santa Barbara</p>
+          ><p class="text-sm inline">- Costa Mesa, CA</p>
           <svg
             class="map-marker"
             xmlns="http://www.w3.org/2000/svg"
@@ -197,16 +196,30 @@
               stroke-linejoin="round"
               stroke-width="2"
               d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-            /></svg
-        ></span>
+            />
+          </svg>
+          <p class="text-sm inline text-blue-300">(Current)</p></span
+        >
       </h3>
       <span>
         <ul class="list-disc pl-4">
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
+          <li>
+            Use Angular and D3.js to create over 30+ reusable visualizations of
+            time series data.
+          </li>
+          <li>
+            Use Python and AWS Sagemaker to create over 1000+ computer vision
+            training data labeling jobs.
+          </li>
+          <li>
+            Develop full stack application consisting of Laravel, Vue.js, and
+            MariaDB. Using Docker/Docker Compose and various services like
+            Nginx, websockets, and PhpMyAdmin.
+          </li>
+          <li>
+            Assist in devops by maintaining migrations/seeders, setting up CI/CD
+            pipeline using Bitbucket actions.
+          </li>
         </ul>
       </span>
     </timeline-event>
