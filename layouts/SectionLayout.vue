8f594ff1b6c9043ad377f0849aad4ca706e13c44
heondo kim <heondokim@gmail.com>
2021-04-13 19:05:27 -0700

    animations

--- layouts/SectionLayout.vue
+++ layouts/SectionLayout.vue
@@ -1,6 +1,12 @@
 <template>
   <section
-    v-observe-visibility="{ once: true, callback: visibilityChanged }"
+    v-observe-visibility="{
+      once: true,
+      callback: visibilityChanged,
+      intersection: {
+        threshold: 0.6,
+      },
+    }"
     :class="{ [bgColor]: true }"
     class="py-12 md:py-16"
   >
@@ -8,21 +14,12 @@
       <h3 v-if="$slots.title" class="text-xl uppercase font-bold mb-4">
         <slot name="title"></slot>
       </h3>
-      <!-- <transition
-        enter-active-class="transition ease-out duration-300"
-        enter-class="transform opacity-0 scale-95"
-        enter-to-class="transform opacity-100 scale-100"
-        leave-active-class="transition ease-in duration-300"
-        leave-class="transform opacity-100 scale-100"
-        leave-to-class="transform opacity-0 scale-95"
-      > -->
       <div
-        class="px-2 sm:px-0 transition transform opacity-0 duration-1000 ease-in-out"
+        class="px-2 sm:px-0 transition transform opacity-0 duration-800 ease-in-out"
         :class="[visible ? 'opacity-100 translate-x-0' : ' -translate-x-6']"
       >
         <slot> </slot>
       </div>
-      <!-- </transition> -->
     </div>
   </section>
 </template>
