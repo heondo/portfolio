e55f93b95a312146ee1be39028db5b3d805f1691
heondo kim <heondokim@gmail.com>
2021-05-04 00:13:24 -0700

    very happy with the progress, articles like a pricing sectino, larger middle section

--- components/AppArticles.vue
+++ components/AppArticles.vue
@@ -1,5 +1,5 @@
 <template>
-  <table class="flex flex-col sm:flex-row gap-2 sm:gap-6 justify-center">
+  <table class="flex flex-col sm:flex-row gap-4 sm:gap-6 justify-center">
     <tr
       v-for="(article, i) in articles"
       :key="article.slug"
@@ -8,16 +8,38 @@
         elevated: articles.length === 3 && i === 1,
       }"
     >
-      <td class="flex flex-col gap-2">
+      <td class="flex flex-col gap-2 h-full">
         <header class="font-semibold">
           {{ article.subject }}
         </header>
-        <h3 class="sm:text-lg">
+        <nuxt-link
+          class="hover:text-gray-400 transition duration-300 ease-in-out font-bold"
+          :to="{ name: 'articles-slug', params: { slug: article.slug } }"
+        >
           {{ article.title }}
-        </h3>
+        </nuxt-link>
         <article>
           {{ article.description }}
         </article>
+        <div class="flex-grow"></div>
+        <nuxt-link
+          class="read-more"
+          :to="{ name: 'articles-slug', params: { slug: article.slug } }"
+        >
+          Read More
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="inline h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </nuxt-link>
       </td>
     </tr>
   </table>
@@ -33,9 +55,11 @@ export default {
 }
 </script>
 <style scoped>
-.elevated {
-  transform: scale(1.17);
-  @apply text-sm;
+@screen sm {
+  .elevated {
+    transform: scale(1.12);
+    @apply text-sm;
+  }
 }
 
 .article-container {
@@ -44,5 +68,13 @@ export default {
   background: -moz-linear-gradient(top, #4f51be, #3c3d9c);
   background: linear-gradient(to bottom, #4f51be, #3c3d9c);
   @apply p-4 rounded-lg shadow-lg;
+}
+
+.read-more {
+  @apply text-right  transition duration-300 ease-in-out;
+}
+
+.read-more:hover {
+  @apply transform -translate-y-1 text-gray-400;
 }
 </style>
